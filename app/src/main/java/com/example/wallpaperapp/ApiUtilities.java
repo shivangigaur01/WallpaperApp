17a83fa6b8e80612a6bc1ddf7826fddef097f05b
Lenova <shivangigaur1002@gmail.com>
2022-10-18 11:13:47 +0530

    WallpaperApp

---
+++ app/src/main/java/com/example/wallpaperapp/ApiUtilities.java
@@ -0,0 +1,17 @@
+package com.example.wallpaperapp;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiUtilities {
+    private static  Retrofit retrofit=null;
+    public static final String API="563492ad6f91700001000001928ed92a416543338e81e2a41c4dc958";
+
+
+    public static ApiInterface getApiInterface(){
+        if(retrofit==null){
+            retrofit=new Retrofit.Builder().baseUrl(ApiInterface.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofit.create(ApiInterface.class);
+    }
+}
